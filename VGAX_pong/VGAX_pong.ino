170e3a56c2a6aebc8cc45aa737cf234ec6664ff3
YruamaLairba <Yruama_Lairba@hotmail.com>
2017-06-07 21:22:32 +0200

    separate command of bars

--- VGAX_pong/VGAX_pong.ino
+++ VGAX_pong/VGAX_pong.ino
@@ -12,26 +12,38 @@ void setup() {
   pinMode(A4, INPUT_PULLUP);
 }
 
-uint8_t cnt;
-unsigned last_millis = 0;
+unsigned last_millis_bars = 0;
 unsigned last_millis_ball = 0;
 
-int8_t bar_y=0;
+//bar 1 position
+const int8_t bar1_x = 0;
+int8_t bar1_y = 0;
 
+//bar 2 position
+const int8_t bar2_x = 119;
+int8_t bar2_y = 0;
+
+//ball position
 int8_t ball_x= VGAX_WIDTH/2;
 int8_t ball_y= VGAX_HEIGHT/2;
 
 //ball speed and direction
-
-
 int8_t ball_dir_x = 1;
 int8_t ball_dir_y = 1;
 
 void loop() {
-  int8_t up = digitalRead(A5);
-  int8_t down = digitalRead(A4);
+
+  //player 1 input
+  int8_t up1 = digitalRead(A5);
+  int8_t down1 = digitalRead(A4);
+
+  //player 1 input
+  int8_t up2 = digitalRead(A5);
+  int8_t down2 = digitalRead(A4);
+  
   unsigned current_millis = vga.millis();
 
+  //ball management
   if ((current_millis - last_millis_ball) >=  50)
   {  
   //vertical border collision
@@ -58,20 +70,19 @@ void loop() {
     last_millis_ball = current_millis;
   }
   
-  
-  if ((current_millis - last_millis) >=  1)
+  //bars management
+  if ((current_millis - last_millis_bars) >=  1)
   {
     vga.clear(0x01);
-    if( up == LOW and  bar_y > 0) bar_y--;
-    if( down == LOW and bar_y < (VGAX_HEIGHT - IMG_BAR_HEIGHT)) bar_y++;
+    if( up1 == LOW and  bar1_y > 0) bar1_y--;
+    if( down1 == LOW and bar1_y < (VGAX_HEIGHT - IMG_BAR_HEIGHT)) bar1_y++;
+    if( up2 == LOW and  bar2_y > 0) bar2_y--;
+    if( down2 == LOW and bar2_y < (VGAX_HEIGHT - IMG_BAR_HEIGHT)) bar2_y++;
 
-    
-    
-    vga.blit((byte*)(img_bar_data[0]), IMG_BAR_WIDTH, IMG_BAR_HEIGHT, 0, bar_y);
-    vga.blit((byte*)(img_bar_data[0]), IMG_BAR_WIDTH, IMG_BAR_HEIGHT, 119, bar_y);
+    vga.blit((byte*)(img_bar_data[0]), IMG_BAR_WIDTH, IMG_BAR_HEIGHT, bar1_x, bar1_y);
+    vga.blit((byte*)(img_bar_data[0]), IMG_BAR_WIDTH, IMG_BAR_HEIGHT, bar2_x, bar2_y);
     vga.blit((byte*)(img_ball_data[0]), IMG_BALL_WIDTH, IMG_BALL_HEIGHT, ball_x, ball_y);
-  //vga.copy((byte*)img_logology_data);
-    last_millis = current_millis;
+    last_millis_bars = current_millis;
   }
 
 }
